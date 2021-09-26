b027d0a03ff9d7ea643d5adab1fb8454cedda066
zonaetmunna <zonaetmonna@gmail.com>
2021-09-26 14:29:48 +0600

    add

--- src/Componant/Ceos/Details/Details.js
+++ src/Componant/Ceos/Details/Details.js
@@ -6,6 +6,7 @@ const Details = (props) => {
      const { details } = props || {};
      // console.log(details);
 
+     // total invest 
      const totalReducer = (privieous, current) => privieous + current.invest;
      const totalInvest = details.reduce(totalReducer, 0).toFixed(2);
 
@@ -13,6 +14,7 @@ const Details = (props) => {
           <div className=" details p-3">
                <h4><i class="fas fa-user"></i>CEOs Added : <span className="text-primary">{details.length}</span></h4>
                <p>Total Invest : <span className="text-primary"> <i class="fas fa-dollar-sign"></i> {totalInvest}</span></p>
+               <h5>Investor name -</h5>
                <div className="text-primary fw-bold">
                     {
                          details.map(detail => <li>{detail.name}</li>)
