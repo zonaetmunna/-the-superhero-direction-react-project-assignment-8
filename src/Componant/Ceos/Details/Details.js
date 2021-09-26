f1ca22fba966824673d34f3fa847b45f99d9f6cc
zonaetmunna <zonaetmonna@gmail.com>
2021-09-26 12:08:26 +0600

    add

--- src/Componant/Ceos/Details/Details.js
+++ src/Componant/Ceos/Details/Details.js
@@ -7,21 +7,31 @@ const Details = (props) => {
      } = props;
      // console.log(details);
 
-     let name = '';
+
+
+
      let totals = 0;
      for (const total of details) {
           // console.log(total)
           totals = totals + total.invest;
           // console.log(total.imgage);
           // image = image + total.imgage;
-          name = name + total.name;
+
      }
 
      return (
           <div className=" details p-3">
                <h4>CEOs Added : <span className="text-primary">{details.length}</span></h4>
                <p>Total Invest : <span className="text-primary"> $ {totals.toFixed(2)}</span></p>
-               <li>{name}</li>
+               <div className="text-primary fw-bold">
+                    {
+                         details.map(detail =>
+                              <li>{detail.name}</li>)
+                    }
+               </div>
+
+
+
           </div>
      );
 };
