9f4f50388b3dd194efaeecdcf415a594bb743838
zonaetmunna <zonaetmonna@gmail.com>
2021-09-26 13:38:44 +0600

    add

--- src/Componant/Ceo/Ceo.js
+++ src/Componant/Ceo/Ceo.js
@@ -4,10 +4,13 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 
 
+
+
 const Ceo = (props) => {
-     const { name, invest, imgage, address, age, company } = props.ceo;
+     const { name, invest, imgage, address, age, company, salary } = props.ceo;
 
      const element = <FontAwesomeIcon icon={faShoppingCart} />
+
 
      return (
           <div className="col-md-4 g-3">
@@ -18,9 +21,14 @@ const Ceo = (props) => {
                          <p className="text-white">Company : {company}</p>
                          <p className="text-white">address : {address}</p>
                          <h6 className="text-white">age: {age}</h6>
+                         <h6 className="text-white"> salary : $ {salary}</h6>
                          <h6 className="text-primary"> Invest-Amount :  {invest}</h6>
                          <br />
                          <button onClick={() => props.handleDetails(props.ceo)} className="text-white bg-primary border border-2 border-info rounded p-1">{element} Add to details</button>
+                         <i class="fas fa-envelope-square text-white ms-3" style={{ height: "10px", width: "20px" }}></i>
+                         <i class="fab fa-facebook-f text-white ms-2"></i>
+                         <i class="fab fa-instagram text-white ms-2"></i>
+                         <i class="fab fa-google text-white ms-2" ></i>
                     </div>
                </div>
           </div>
