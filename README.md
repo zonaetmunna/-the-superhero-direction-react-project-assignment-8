cacc9aba055ad337d2396f3f0114ded65c1e0d37
zonaetmunna <zonaetmonna@gmail.com>
2021-09-26 12:51:08 +0600

    add

--- README.md
+++ README.md
@@ -3,9 +3,7 @@
 
 Go to our live site:  (https://best-ceo-team-815428.netlify.app/).
 
-## Available Scripts
 
-In the project directory, you can run:
 ### Advntage
 * It give you for future IT business
 * It can be change your future
--- src/Componant/Ceos/Details/Details.js
+++ src/Componant/Ceos/Details/Details.js
@@ -3,30 +3,20 @@ import React from 'react';
 import './Details.css'
 
 const Details = (props) => {
-     const { details
-     } = props;
+     const { details } = props || {};
      // console.log(details);
 
-
-
-
-     let totals = 0;
-     for (const total of details) {
-          // console.log(total)
-          totals = totals + total.invest;
-          // console.log(total.imgage);
-          // image = image + total.imgage;
-
-     }
+     const totalReducer = (privieous, current) => privieous + current.invest;
+     const totalInvest = details.reduce(totalReducer, 0).toFixed(2);
 
      return (
           <div className=" details p-3">
                <h4>CEOs Added : <span className="text-primary">{details.length}</span></h4>
-               <p>Total Invest : <span className="text-primary"> $ {totals.toFixed(2)}</span></p>
+               <p>Total Invest : <span className="text-primary"> $ {totalInvest}</span></p>
                <div className="text-primary fw-bold">
                     {
-                         details.map(detail =>
-                              <li>{detail.name}</li>)
+                         details.map(detail => <li>{detail.name}</li>)
+
                     }
                </div>
 
